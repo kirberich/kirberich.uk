003097aae88a72c834805c7dd60428353b620cfd
Robert Kirberich <robert@kirberich.de>
2015-01-27 20:04:13 +0000

    Add more image options, image uploading

--- core/models.py
+++ core/models.py
@@ -8,8 +8,11 @@ class Article(models.Model):
     name = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
     is_dark = models.BooleanField(default=False)
+    background_img = models.FileField(blank=True, upload_to="/upload")
     background_img_url = models.CharField(max_length=500, blank=True)
     background_img_extra_css = models.CharField(max_length=500, blank=True)
+
+    hero_img = models.FileField(blank=True, upload_to="/upload")
     hero_img_url = models.CharField(max_length=500, blank=True)
     is_hero_portrait = models.BooleanField(default=False)
     hero_link_url = models.CharField(max_length=500, blank=True)
@@ -18,3 +21,17 @@ class Article(models.Model):
 
     def __unicode__(self):
         return self.name
+
+    def get_background_url(self):
+        if self.background_img.name:
+            return self.background_img.url
+        if self.background_img_url:
+            return self.background_img_url
+        if self.hero_img.name:
+            return self.hero_img.url
+        return self.hero_img_url
+
+    def get_hero_url(self):
+        if self.hero_img.name:
+            return self.hero_img.url
+        return self.hero_img_url
--- kirberichuk/settings.py
+++ kirberichuk/settings.py
@@ -104,6 +104,9 @@ USE_L10N = True
 USE_TZ = True
 
 
+FILE_UPLOAD_MAX_MEMORY_SIZE = 1024 * 1024 * 30
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
